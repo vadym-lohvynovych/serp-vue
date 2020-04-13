5ebe226dbf9ba8c10305768ef347ac50a1a11d95
Vado <vadvoker@gmail.com>
2020-04-13 18:13:46 +0300

    add vuex and default comics state

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,5 +1,13 @@
 <template>
-	<div class="container">
-		App
+	<div class="container max-width-1200 mx-auto">
+		<h1 class="text-center font-bold text-xl">App</h1>
 	</div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+	computed: mapGetters(['allComics'])
+}
+</script>
---
+++ src/app/store/index.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import comics from "./comics";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+	modules: {
+		comics
+	}
+});
