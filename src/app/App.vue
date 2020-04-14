6391f8467146806bb469ac047dcaf7ab0ef750c2
madvingovich <37845039+madvingovich@users.noreply.github.com>
2020-04-14 09:41:20 +0300

    add search bar and loader (#3)
    * use prettier
    
    * remane scss file to tailwind
    
    * add search bar and loader
    
    * fixes from comments in pull request
    
    * use two spaces for an indent
    
    * api module in utils intead of class
    
    * move utils to src

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,13 +1,38 @@
 <template>
-	<div class="container max-width-1200 mx-auto">
-		<h1 class="text-center font-bold text-xl">App</h1>
-	</div>
+  <div class="container mx-auto">
+    <h1 class="text-center text-xl my-5 font-hairline">Here you can find some comics info!</h1>
+
+    <SearchBar />
+
+    <Loader v-if="isLoading" />
+
+    <p v-else-if="searchResult && searchResult.length" class="text-center font-hairline my-8">
+      <span>{{ searchResult.length }}</span>
+    </p>
+
+    <p v-else-if="searchResult" class="text-center font-hairline my-8">Cant find :(</p>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
+import SearchBar from "./components/SearchBar.vue";
+import Loader from "./components/Loader.vue";
 
 export default {
-	computed: mapGetters(['allComics'])
-}
+  computed: mapGetters(["searchResult", "isLoading"]),
+  components: {
+    SearchBar,
+    Loader
+  }
+};
 </script>
+
+
+<style lang="scss">
+body {
+  min-height: 100vh;
+  background: linear-gradient(to right bottom, #323d42 0%, #191e24 100%);
+  color: #fff;
+}
+</style>
--- src/app/store/comics.js
+++ src/app/store/comics.js
@@ -1,14 +1,47 @@
+import { searchComics } from '../../utils/api'
+
 export default {
-    actions: {},
-    mutations: {},
-    state: {
-        items: [],
-        fetching: false,
-        error: false
-    },
-    getters: {
-        allComics(state) {
-            return state.items
-        }
+  actions: {
+    findComics(context, title) {
+      context.commit('setFetching', true);
+      searchComics(title)
+        .then(res => {
+          context.commit('updateComics', res.data);
+        })
+        .catch(error => {
+          context.commit('setError', error);
+        })
+        .finally(() => {
+          context.commit('setFetching', false);
+        });
     }
-}
+  },
+  mutations: {
+    setFetching(state, value) {
+      state.fetching = value;
+    },
+    updateComics(state, comics) {
+      const { results, ...rest } = comics;
+      state.comics = {
+        items: results,
+        ...rest
+      };
+    },
+    setError(state, error) {
+      state.error = error;
+    }
+  },
+  state: {
+    comics: null,
+    fetching: false,
+    error: false
+  },
+  getters: {
+    searchResult(state) {
+      return state.comics && state.comics.items;
+    },
+    isLoading(state) {
+      return state.fetching;
+    }
+  }
+};
