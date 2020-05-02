0ecb39b8ef125d3c3c67ae1bef9d6e77425d1dac
Vado <vadvoker@gmail.com>
2020-05-02 16:57:25 +0300

    remove comments from apiStructure file

--- src/utils/apiStructure.js
+++ src/utils/apiStructure.js
@@ -1,24 +1,4 @@
 export default {
-  // comics: {
-  //   search: 'searchComics',
-  //   get: 'getComicsById'
-  // },
-  // characters: {
-  //   search: 'searchCharacters',
-  //   get: 'getCharacterById'
-  // },
-  // stories: {
-  //   get: 'getStoryById'
-  // },
-  // series: {
-  //   get: 'getSeriesById'
-  // },
-  // creators: {
-  //   get: 'getCreatorById'
-  // },
-  // all: {
-  //   search: 'getRandomCharacters'
-  // },
   getById: {
     comics: 'getComicsById',
     characters: 'getCharacterById',
