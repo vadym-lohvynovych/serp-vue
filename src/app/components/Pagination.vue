0a965dd872862e39dd81c279f4eabb9a7faa3822
Vado <vadvoker@gmail.com>
2020-05-06 16:50:04 +0300

    add pagination logic

--- src/app/components/Pagination.vue
+++ src/app/components/Pagination.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="flex items-center items-center justify-center py-6">
-    <p
-      v-for="n in pagesCount"
-      :key="n"
-      @click="changePage(n)"
-      :class="{active: isActive(n), 'bg-indigo-600': isActive(n)}"
-      class="py-1 px-2 rounded mx-1 bg-indigo-300 text-black shadow-xl cursor-pointer hover:bg-indigo-500 with-transition"
-    >{{ n }}</p>
+  <div>
+    <div v-if="pagesCount < 7" class="flex items-center items-center justify-center py-6">
+      <p
+        v-for="n in pagesCount"
+        :key="n"
+        @click="changePage(n)"
+        :class="paginationClassObject(n)"
+      >{{ n }}</p>
+    </div>
+
+    <div v-else class="flex items-center items-center justify-center py-6">
+      <div v-for="(page, idx) in pagesArray" :key="idx">
+        <p
+          v-if="typeof page === 'number'"
+          @click="changePage(page)"
+          :class="paginationClassObject(page)"
+        >{{ page }}</p>
+        <p v-else-if="page === 'dots' && shoultRenderDots(idx)" class="py-1 px-2 mx-1">...</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -26,7 +38,43 @@ export default {
     },
 
     currentPage() {
-      return this.$route.query.page || 1;
+      return Number(this.$route.query.page) || 1;
+    },
+
+    pagesArray() {
+      if (this.currentPage <= 3) {
+        return [
+          1,
+          2,
+          3,
+          4,
+          'dots',
+          this.pagesCount - 2,
+          this.pagesCount - 1,
+          this.pagesCount
+        ];
+      } else if (this.currentPage >= this.pagesCount - 2) {
+        return [
+          1,
+          'dots',
+          this.pagesCount - 3,
+          this.pagesCount - 2,
+          this.pagesCount - 1,
+          this.pagesCount
+        ];
+      } else {
+        return [
+          1,
+          'dots',
+          this.currentPage - 2,
+          this.currentPage - 1,
+          this.currentPage,
+          this.currentPage + 1,
+          this.currentPage + 2,
+          'dots',
+          this.pagesCount
+        ];
+      }
     }
   },
 
@@ -42,6 +90,22 @@ export default {
         title,
         offset: (pageNumber - 1) * this.limit
       });
+    },
+
+    paginationClassObject(n) {
+      return {
+        active: this.isActive(n),
+        'bg-indigo-600': this.isActive(n),
+        'py-1 px-2 rounded mx-1 bg-indigo-300 text-black shadow-xl cursor-pointer hover:bg-indigo-500 with-transition': true
+      };
+    },
+
+    shoultRenderDots(dotsIndex) {
+      if (dotsIndex === 1) {
+        return this.pagesArray[dotsIndex + 1] !== 2;
+      } else {
+        return this.pagesArray[dotsIndex - 1] !== this.pagesCount - 1;
+      }
     },
 
     isActive(n) {
